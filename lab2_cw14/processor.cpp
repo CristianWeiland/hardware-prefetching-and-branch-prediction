bf9443036a468f4aad5a0295d9cae06bd8b161c9
Cristian Weiland <cw14@inf.ufpr.br>
2017-10-01 13:29:51 -0300

    Add working BTB
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

---
+++ lab2_cw14/processor.cpp
@@ -0,0 +1,199 @@
+#include "simulator.hpp"
+
+//row Btb[SETS / WAYS][WAYS];
+row *Btb;
+
+unsigned int Hit, Miss, BtbHit, BtbMiss;
+
+inline int idx(int base, int deslocamento) {
+    return base * WAYS + deslocamento;
+}
+
+inline int getBase(uint64_t pc) {
+    return (pc >> 2) & TAG_BITS;
+}
+
+row* inBtb(uint64_t pc) {
+// If we find a row in Btb with this pc and valid == true, return it.
+// Otherwise return NULL.
+    int base = getBase(pc);
+    int i, index = -1;
+    for (i=0; i<WAYS; ++i) {
+        if (Btb[idx(base,i)].address == pc && Btb[idx(base,i)].valid) {
+            index = i;
+        }
+    }
+    if (index > 1) {
+        return &(Btb[idx(base, index)]);
+    }
+    return NULL;
+}
+
+row createRow(uint64_t address, int opcode_size) {
+    row newRow;
+    newRow.address = address;
+    newRow.tag = getBase(address);
+    newRow.idx = -1;
+    newRow.lru = 0;
+    newRow.valid = true;
+    newRow.bht = -1;
+    newRow.target_address = 0;
+    newRow.opcode_size = opcode_size;
+    return newRow;
+}
+
+// BTB
+void copy_row(row *dst, row src) {
+    dst->tag = src.tag;
+    dst->idx = src.idx;
+    dst->lru = src.lru;
+    dst->address = src.address;
+    dst->valid = src.valid;
+    dst->bht = src.bht;
+    dst->target_address = src.target_address;
+    dst->opcode_size = src.opcode_size;
+}
+
+void insert_row(row newRow) {
+    int base = newRow.tag;
+
+    int i, invalid = -1, menor_lru = WAYS+1, index = -1;
+    // Precisamos decidir quem é o cara que eu vou chutar fora.
+    // Se achar alguem inválido, é ele. Se não, pega o cara com
+    // menor LRU.
+    for (i=0; i<WAYS; ++i) {
+        if (!(Btb[idx(base, i)]).valid) {
+            invalid = i;
+            break;
+        }
+        if (Btb[idx(base,i)].lru < menor_lru) {
+            menor_lru = Btb[idx(base,i)].lru;
+            index = i;
+        }
+    }
+    if (invalid != -1) {
+        index = invalid;
+    }
+    newRow.valid = true;
+    newRow.lru = WAYS-1;
+    // Nesse momento eu tenho certeza que idx contém o index certo.
+    copy_row(&(Btb[idx(base, index)]), newRow);
+
+    // Atualiza LRUs
+    for (i=0; i<WAYS; ++i) {
+        --Btb[idx(base,i)].lru;
+    }
+}
+
+// =====================================================================
+processor_t::processor_t() {
+
+};
+
+// =====================================================================
+void processor_t::allocate() {
+    Btb = (row*) malloc(sizeof(struct row) * N_ROWS * WAYS);
+    int i;
+    for (i=0; i<N_ROWS*WAYS; ++i) {
+        Btb[i].valid = false;
+    }
+    Hit = 0;
+    Miss = 0;
+    BtbHit = 0;
+    BtbMiss = 0;
+};
+
+// =====================================================================
+void processor_t::clock() {
+    static bool wasBranch = false, predicted = false;
+    static uint64_t previous_pc = 0, predicted_pc = 0;
+
+	/// Get the next instruction from the trace
+	opcode_package_t new_instruction;
+	if (!orcs_engine.trace_reader->trace_fetch(&new_instruction)) {
+		/// If EOF
+		orcs_engine.simulator_alive = false;
+        return;
+	}
+
+    // Checa se a instrucao anterior era um branch. Se sim, atualiza o target_address dela na BTB.
+    if (wasBranch) {
+        int i, index = 0, base = getBase(previous_pc);
+        for (i=0; i<WAYS; ++i) {
+            if (Btb[idx(base,i)].address == previous_pc) {
+                index = i;
+            }
+        }
+        // Btb[idx(base, index)] tells us the row in BTB to the previous instr.
+        Btb[idx(base,index)].target_address = new_instruction.opcode_address;
+
+        if (Btb[idx(base,index)].address + Btb[idx(base,index)].opcode_size == new_instruction.opcode_address) {
+            // Se o pc atual eh o pc anterior + tam da instrucao anterior, not taken.
+            Btb[idx(base,index)].bht = 0;
+        } else {
+            // Se nao foi nao taken, foi taken.
+            Btb[idx(base,index)].bht = 1;
+        }
+
+        if (predicted) {
+            if (new_instruction.opcode_address == predicted_pc) {
+                ++Hit;
+            } else {
+                ++Miss;
+            }
+        }
+
+        wasBranch = false;
+        previous_pc = 0;
+        predicted = false;
+        predicted_pc = 0;
+    }
+
+    /*
+    Estou supondo que nao preciso efetivamente dar o rollback nem alterar o pc.
+    Soh vou contar quantos eu teria acertado se eu tivesse aplicado a previsao.
+    E quantos teria errado.
+    Pra fazer isso, toda vez que eu tentar prever, vou marcar predicted como true
+    e salvar em predicted_pc o pc que eu acredito que seria o certo. Na proxima
+    instrucao eu comparo predicted_pc com a instrucao atual. Se for igual, ++hit.
+    Se nao, ++miss.
+    */
+
+    if (new_instruction.opcode_operation == INSTRUCTION_OPERATION_BRANCH) { // It is a branch
+        predicted = false;
+        row *branchRow = inBtb(new_instruction.opcode_address);
+
+        if (branchRow && branchRow->valid) { // We have valid information about it in our BTB
+            if (branchRow->bht == 0) { // Last time, branch was not taken.
+                BtbHit++;
+                predicted_pc = new_instruction.opcode_address + new_instruction.opcode_size;
+                predicted = true;
+            } else if(branchRow->bht == 1) { // Last time, branch was taken.
+                if (branchRow->target_address != 0) { // Branch was taken. Do we have an address to go?
+                    BtbHit++;
+                    predicted_pc = branchRow->target_address;
+                    predicted = true;
+                } else {
+                    printf("This should NOT have happened...\n");
+                }
+            }
+        } else { // We didnt have valid information. Add a new row in our BTB.
+            BtbMiss++;
+            insert_row(createRow(new_instruction.opcode_address, new_instruction.opcode_size));
+        }
+
+        // Sinalize that in the next instruction we have to update target_address.
+        wasBranch = true;
+        previous_pc = new_instruction.opcode_address;
+    }
+};
+
+// =====================================================================
+void processor_t::statistics() {
+	ORCS_PRINTF("######################################################\n");
+	ORCS_PRINTF("processor_t\n");
+    ORCS_PRINTF("Branch Hits: %d\n", Hit);
+    ORCS_PRINTF("Branch Misses: %d\n", Miss);
+    ORCS_PRINTF("BTB Hits: %d\n", BtbHit);
+    ORCS_PRINTF("BTB Misses: %d\n", BtbMiss);
+};
---
+++ lab2_cw14/processor.hpp
@@ -0,0 +1,61 @@
+#ifndef PROCESSOR_HPP
+#define PROCESSOR_HPP
+
+// ============================================================================
+// ============================================================================
+class processor_t {
+    private:    
+    
+    
+    public:
+
+		// ====================================================================
+		/// Methods
+		// ====================================================================
+		processor_t();
+	    void allocate();
+	    void clock();
+	    void statistics();
+};
+
+// ============================================================================
+/// BTB Defines
+// ============================================================================
+#define SETS 512
+#define WAYS 4 // N_WAY_SET_ASSOCIATIVE
+#define N_ROWS (SETS/WAYS)
+#define TAG_BITS (N_ROWS - 1)
+
+struct row {
+    int tag; // 7 bits do PC (Instruction Address)
+    int idx; // Possivelmente não precisa salvar na row. É o PC depois dos cálculos (>> 2 & TAG_BITS).
+    int opcode_size; // Tamanho da instrucao
+    uint8_t lru;
+    uint64_t address; // PC Completo
+    bool valid;
+    int8_t bht; // Armazena a ultima direção que o salto foi tomado.
+                // Se eu já tiver, eu posso prever chutando essa direção.
+    uint64_t target_address; // Calculando: coloca o endereço da próxima instrução quando der o fetch da próxima.
+};
+
+typedef struct row row;
+
+int idx(int base, int deslocamento);
+void copy_row(row *dst, row src);
+void insert_row(row *btb, row newRow);
+
+// Definindo index na btb a partir do pc:
+// PC shift pra direita (ignorar 2 (log(N_WAY_SET_ASSOCIATIVE)) LSB)
+// 512 entradas, preciso log2(512 / 4) bits = 7.
+// Pra pegar só esses bits, faz um bitwise and (&) com 127 (1111111).
+// Achei a base. Agora tenho que descobrir se alguma das 4 entradas tá
+// vazia. Se não tiver, joga fora a mais velha (a partir do LRU).
+
+// Pra saber se houve um branch ou não, tenho que pegar o OPCODE_ADDRESS e somar com OPCODE_SIZE.
+// Se o endereço da próxima instrução for OPCODE_ADDRESS + OPCODE_SIZE, branch not taken, se não,
+// taken.
+
+// btb = lista de conjuntos;
+// conjuntos = lista de linhas;
+
+#endif
